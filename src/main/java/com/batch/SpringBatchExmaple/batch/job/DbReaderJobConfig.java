07a288ded66cb6cf7474e3566cea3cb681873ca7
memorykghs <37746042+memorykghs@users.noreply.github.com>
2021-11-25 08:40:37 +0800

    202111250840

--- src/main/java/com/batch/SpringBatchExmaple/batch/job/DbReaderJobConfig.java
+++ src/main/java/com/batch/SpringBatchExmaple/batch/job/DbReaderJobConfig.java
@@ -34,7 +34,7 @@ import com.batch.SpringBatchExmaple.entity.Cars;
 import com.batch.SpringBatchExmaple.repository.CarsRepo;
 
 /**
- * DB -> File
+ * 清除 Log 批次設定
  * 
  * @author memorykghs
  */
@@ -49,19 +49,6 @@ public class DbReaderJobConfig {
 	@Autowired
 	private StepBuilderFactory stepBuilderFactory;
 
-	/** CarRepo */
-	@Autowired
-	private CarsRepo carRepo;
-
-	/** 每批件數 */
-	private static int FETCH_SIZE = 10;
-	
-	/** Mapper Field */
-    private static final String[] MAPPER_FIELD = new String[] { "Manufacturer", "Type", "Spread" }; // TODO 欄位大小寫有關係?用什麼做mapping?
-
-    /** Header */
-    private final String HEADER = new StringBuilder().append("製造商").append(',').append("類別").append(',').append("價差").toString();
-
 	/**
 	 * 建立 Job
 	 * 
@@ -69,7 +56,6 @@ public class DbReaderJobConfig {
 	 * @return
 	 */
 	@Bean
-//	public Job dbReaderJob(@Qualifier("Db001Step") Step step) {
 	public Job dbReaderJob(@Qualifier("clearLogStep") Step step) {
 		return jobBuilderFactory.get("Db001Job")
 				.start(step)
@@ -83,74 +69,6 @@ public class DbReaderJobConfig {
 		return stepBuilderFactory.get("Db001Step")
 				.transactionManager(transactionManager)
 				.tasklet(new ClearLogTasklet())
-				.build();
-	}
-
-	/**
-	 * 建立 Step
-	 * 
-	 * @param itemReader
-	 * @param itemWriter
-	 * @param transactionManager
-	 * @return
-	 */
-	@Bean("Db001Step")
-	public Step dbReaderStep(@Qualifier("Db001JpaReader") ItemReader<Cars> itemReader, @Qualifier("Db001FileWriter") ItemWriter<CarsDto> itemWriter,
-			ItemProcessor<Cars, CarsDto> processor, JpaTransactionManager transactionManager) {
-
-		return stepBuilderFactory.get("Db001Step")
-				.transactionManager(transactionManager)
-				.<Cars, CarsDto>chunk(FETCH_SIZE)
-				.faultTolerant()
-//                .skip(Exception.class)
-//                .skipLimit(Integer.MAX_VALUE)
-				.reader(itemReader)
-				.processor(processor)
-				.writer(itemWriter)
-				.listener(new Db001StepListener())
-				.listener(new Db001ReaderListener())
-				.listener(new Db001WriterListener())
-				.build();
-	}
-
-	/**
-	 * 建立 Jpa Reader
-	 * 
-	 * @return
-	 */
-	@Bean("Db001JpaReader")
-	public RepositoryItemReader<CarsDto> itemReader() {
-
-		Map<String, Direction> sortMap = new HashMap<>();
-		sortMap.put("Manufacturer", Direction.ASC);
-		sortMap.put("Type", Direction.ASC);
-
-		return new RepositoryItemReaderBuilder<CarsDto>()
-				.name("Db001JpaReader")
-				.pageSize(FETCH_SIZE)
-				.repository(carRepo)
-				.methodName("findAll")
-				.sorts(sortMap)
-				.build();
-	}
-
-	/**
-	 * 建立 File Writer
-	 * @return
-	 */
-	@Bean("Db001FileWriter")
-	public FlatFileItemWriter<Cars> customFlatFileWriter() {
-
-		String fileName = new SimpleDateFormat("yyyyMMddHHmmssS").format(new Date());
-
-		return new FlatFileItemWriterBuilder<Cars>().name("Db001FileWriter")
-				.encoding("UTF-8")
-				.resource(new FileSystemResource("D:/" + fileName + ".csv"))
-				.append(true) // 是否串接在同一個檔案後
-				.delimited()
-				.names(MAPPER_FIELD)
-//				.shouldDeleteIfEmpty(true) // 當檔案存在且內容為空，restart時會重新生產一份
-				.headerCallback(headerCallback -> headerCallback.write(HEADER)) // 使用 headerCallback 寫入表頭
 				.build();
 	}
 }
