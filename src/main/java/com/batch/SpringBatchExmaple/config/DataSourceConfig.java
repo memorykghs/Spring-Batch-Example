6014225ab30c2a6373e4fbef982f786016c75f98
memorykghs <37746042+memorykghs@users.noreply.github.com>
2022-01-05 11:08:39 +0800

    Update DataSourceConfig.java

--- src/main/java/com/batch/SpringBatchExmaple/config/DataSourceConfig.java
+++ src/main/java/com/batch/SpringBatchExmaple/config/DataSourceConfig.java
@@ -1,34 +1,23 @@
-package com.batch.SpringBatchExmaple.config;
+package com.batch.SpringBatchExample.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 
-
-//@Configuration
-//@Order(1)
+@Configuration
 public class DataSourceConfig {
-//    @Value("${DBUrl}")
-//    private String dataSourceUrl;
-//    
-//    @Value("${cub.aco.db.onsConfiguration:N}")
-//    private String onsConfiguration;
 
-//    @Bean(name = "FXSLOCAL")
-//    @Primary
-//    @Profile("local")
-//    public DataSource getLocalDataSource() throws SQLException {
-
-//        PoolDataSource poolDataSource = PoolDataSourceFactory.getPoolDataSource();
-//
-//        poolDataSource.setUser("OTRLXFXS01");
-//        poolDataSource.setPassword("OTRLXFXS01");
-//        poolDataSource.setURL(dataSourceUrl);
-//        poolDataSource.setConnectionFactoryClassName("oracle.jdbc.pool.OracleDataSource");
-//        poolDataSource.setFastConnectionFailoverEnabled(true);
-//        poolDataSource.setValidateConnectionOnBorrow(true); // boolean, 放true
-//        poolDataSource.setSQLForValidateConnection("select 1 from dual");
-//
-//        return poolDataSource;
-//    }
+    @Bean
+    @ConfigurationProperties(prefix = "oracle.db.student")
+    public DataSourceProperties dataSourceProperties() {
+        return new DataSourceProperties();
+    }
+    
+    @Bean
+    public DataSource dataSource() {
+        return dataSourceProperties().initializeDataSourceBuilder().build();
+    }
 }
