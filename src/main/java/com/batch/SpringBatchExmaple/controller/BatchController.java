0b10778c10b1b21aff6100d025583a73f1f952ac
memorykghs <37746042+memorykghs@users.noreply.github.com>
2022-01-05 11:09:16 +0800

    Update BatchController.java

--- src/main/java/com/batch/SpringBatchExmaple/controller/BatchController.java
+++ src/main/java/com/batch/SpringBatchExmaple/controller/BatchController.java
@@ -1,138 +1,80 @@
-package com.batch.SpringBatchExmaple.controller;
+package com.batch.SpringBatchExample.controller;
 
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.Date;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
-import org.springframework.batch.core.UnexpectedJobExecutionException;
 import org.springframework.batch.core.configuration.JobRegistry;
 import org.springframework.batch.core.launch.JobInstanceAlreadyExistsException;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.JobOperator;
-import org.springframework.batch.core.launch.JobParametersNotFoundException;
-import org.springframework.batch.core.launch.NoSuchJobException;
-import org.springframework.batch.core.launch.NoSuchJobExecutionException;
-import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
-import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
-import org.springframework.batch.core.repository.JobRestartException;
-import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-
-@Api(tags = "Spring Batch Examples")
 @RestController
-public class BatchController {
+public class BatchController2 {
+    
+    private static final Logger LOGGER = LoggerFactory.getLogger(BatchController2.class);
+    
+    @Autowired
+    private JobOperator jobOperator;
+    
+    @Autowired
+    private JobRegistry jobRegistry;
 
-	/** LOG */
-	private static final Logger LOGGER = LoggerFactory.getLogger(BatchController.class);
-
-	/** JobOperator */
-	@Autowired
-	private JobOperator jobOperator;
-
-	@Autowired
-	private JobRegistry jobRegistry;
-
-	@Autowired
-	private JobLauncher jobLauncher;
-
-	/**
-	 * call by web conatiner
-	 * 
-	 * @throws JobExecutionAlreadyRunningException
-	 * @throws JobRestartException
-	 * @throws JobInstanceAlreadyCompleteException
-	 * @throws JobParametersInvalidException
-	 */
-	@ApiOperation(value = "執行讀DB批次 (JobLauncher)")
-	@RequestMapping(value = "/dbReader001Job", method = RequestMethod.POST)
-	public String doDbReader001Job() {
-
-		try {
-
-			JobParametersBuilder builder = new JobParametersBuilder();
-			builder.addString("sameParameter", "string");
-
-//			jobLauncher.run(jobRegistry.getJob("Db001Job"), new JobParameters());
-			jobLauncher.run(jobRegistry.getJob("Db001Job"), builder.toJobParameters());
-//			jobLauncher.run(jobRegistry.getJob("Db001Job"), createJobParams("Db001Job"));
-
-		} catch (JobExecutionAlreadyRunningException jobExecutionAlreadyRunningException) {
-			LOGGER.info("Job execution is already running.");
-
-		} catch (JobRestartException jobRestartException) {
-			LOGGER.info("Job restart exception happens.");
-
-		} catch (JobInstanceAlreadyCompleteException jobInstanceAlreadyCompleteException) {
-			LOGGER.info("Job instance is already completed.");
-
-		} catch (JobParametersInvalidException jobParametersInvalidException) {
-			LOGGER.info("Job parameters invalid exception");
-
-		} catch (BeansException beansException) {
-			LOGGER.info("Bean is not found.");
-
-		} catch (NoSuchJobException e) {
-			e.printStackTrace();
-		}
-
-		return "finished";
-	}
-
-	/**
-	 * 使用 JobOperator
-	 * 
-	 * @return
-	 */
-	@ApiOperation(value = "執行讀DB批次 (JobOperator)")
-	@RequestMapping(value = "/dbReader001Job2", method = RequestMethod.POST)
-	public String doDbReader001Job2() {
-
-		String summary = null;
-
-		try {
-//			long jobExecutionId = jobOperator.start("Db001Job", "1"); // 取得 JobExecutionId
-			long jobExecutionId = jobOperator.startNextInstance("Db001Job");
-			summary = jobOperator.getSummary(jobExecutionId);
-
-		} catch (NoSuchJobException | JobParametersInvalidException e) {
-			e.printStackTrace();
-		} catch (NoSuchJobExecutionException e) {
-			e.printStackTrace();
-		} catch (JobParametersNotFoundException e) {
-			e.printStackTrace();
-		} catch (JobRestartException e) {
-			e.printStackTrace();
-		} catch (JobExecutionAlreadyRunningException e) {
-			e.printStackTrace();
-		} catch (JobInstanceAlreadyCompleteException e) {
-			e.printStackTrace();
-		} catch (UnexpectedJobExecutionException e) {
-			e.printStackTrace();
-		}
-
-		return summary;
-	}
-
-	/**
-	 * 產生JobParameter
-	 * 
-	 * @return
-	 */
-	private JobParameters createJobParams(String jobName) {
-
-		JobParametersBuilder builder = new JobParametersBuilder();
-		builder.addDate("executeTime", Timestamp.valueOf(LocalDateTime.now()));
-
-		return builder.toJobParameters();
-	}
+    @Autowired
+    private JobLauncher jobLauncher;
+    
+    @GetMapping(value = "/testGroupItem")
+    public void testGroupItem() throws Exception {
+        try {
+            String params = new StringBuilder()
+                    .append("today=20211109").append(',')
+                    .append("tomorrow=20211110")
+                    .toString();
+            jobOperator.start("testGroupItem", params);
+        } catch (JobInstanceAlreadyExistsException e) {
+            LOGGER.debug("Job實例已存在，產生新的實例繼續執行", e);
+            jobOperator.startNextInstance("testGroupItem");
+        }
+    }
+    
+    /**
+     * <pre>
+     * 和testColumnMapRowMapperJob2啟動相同的Job
+     * 用來測試Process的StepScope
+     * </pre>
+     * @throws Exception
+     */
+    @GetMapping(value = "/testColumnMapRowMapperJob")
+    public void testColumnMapRowMapperJob() throws Exception {
+        Job job = jobRegistry.getJob("testColumnMapRowMapper");
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("exeDate", LocalDate.now().toString())
+                .addDate("exeTime", new Date(), true)
+                .toJobParameters();
+        jobLauncher.run(job, jobParameters);
+    }
+    
+    /**
+     * <pre>
+     * 和testColumnMapRowMapperJob啟動相同的Job
+     * 用來測試Process的StepScope
+     * </pre>
+     * @throws Exception
+     */
+    @GetMapping(value = "/testColumnMapRowMapperJob2")
+    public void testColumnMapRowMapperJob2() throws Exception {
+        Job job = jobRegistry.getJob("testColumnMapRowMapper");
+        JobParameters jobParameters = new JobParametersBuilder()
+                .addString("exeDate", LocalDate.now().plusDays(1).toString())
+                .addDate("exeTime", new Date(), true)
+                .toJobParameters();
+        jobLauncher.run(job, jobParameters);
+    }
 }
